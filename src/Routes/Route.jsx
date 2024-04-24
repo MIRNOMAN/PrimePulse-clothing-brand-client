df1f1946536a9440261f5392d93ee014ba339290
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2024-04-25 03:04:37 +0600

    create a footer section

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import { MainLayout } from "../Layout/MainLayout";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Shop from "../Pages/Shop/Shop";
+import Men from "../Pages/Men/Men";
+import Women from "../Pages/Women/Women";
+import Kids from "../Pages/Kids/Kids";
 
 
 
@@ -14,6 +17,18 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Shop></Shop>,
+            },
+            {
+                path: '/men',
+                element: <Men></Men>,
+            },
+            {
+                path: '/women',
+                element: <Women></Women>,
+            },
+            {
+                path: '/kids',
+                element: <Kids></Kids>,
             },
         ]
 
--- src/Share/Footer/Footer.jsx
+++ src/Share/Footer/Footer.jsx
@@ -1,9 +1,38 @@
+import footer_logo from "../../Images/logo (2).png"
+import instagram_icon from "../../Images/instagram_icon.png"
+import pintexter_icon from "../../Images/pintester_icon.png"
+import whatsApp_icon from "../../Images/whatsapp_icon.png"
 
 
 const Footer = () => {
   return (
-    <div className="">
-      
+    <div className="flex flex-col justify-center items-center gap-6">
+       <div className="flex items-center justify-center gap-5">
+        <img className="h-28" src={footer_logo} alt="" />
+        <p className="text-[#383838] mt-5 text-4xl font-bold">SHOPPER</p>
+       </div>
+       <ul className="flex list-none gap-12 text-[#252525] text-lg">
+         <li className="cursor-pointer">Company</li>
+         <li className="cursor-pointer">Product</li>
+         <li className="cursor-pointer"> Office</li>
+         <li className="cursor-pointer">About</li>
+         <li className="cursor-pointer">Contact</li>
+       </ul>
+       <div className="flex gap-12">
+        <div className="p-3 pb-[6px] bg-[#fbfbfb] footer-container">
+          <img src={instagram_icon} alt="" />
+        </div>
+        <div className="p-3 pb-[6px] bg-[#fbfbfb] footer-container">
+          <img src={pintexter_icon} alt="" />
+        </div>
+        <div className="p-3 pb-[6px] bg-[#fbfbfb] footer-container">
+          <img src={whatsApp_icon} alt="" />
+        </div>
+       </div>
+       <div className="flex flex-col items-center gap-7 w-[100%] mb-[30px] text-[#1a1a1a] text-lg">
+        <hr className="w-[80%] border-none rounded-lg h-[3px] bg-[#c7c7c7]" />
+        <p>Copyright @ 2024 - All Right Reserves.</p>
+       </div>
     </div>
   )
 }
