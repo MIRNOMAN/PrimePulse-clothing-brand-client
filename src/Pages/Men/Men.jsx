3427464b45ad697ce2a48dc57e05da32121b5d8f
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2024-04-26 12:28:08 +0600

    create man products map and show more button

--- src/Pages/Men/Men.jsx
+++ src/Pages/Men/Men.jsx
@@ -1,9 +1,35 @@
 import men_banner from "../../Images/banner_mens.png";
 
+import dropdown_icon from '../../Images/dropdown_icon.png'
+import Item from "../../Components/Items/Items"
+import { useContext } from "react";
+import { ShopContext } from "../../Context/ShopContext";
+
 const Men = () => {
+  const { all_products } = useContext(ShopContext);
   return (
     <div className="shop-category">
-       <img src={men_banner} alt="" />
+      <img className="block  w-[77%]" src={men_banner} alt="" />
+      <div className="flex m-[0px 170px] justify-between items-center">
+        <p>
+          <span className="font-semibold">Showing 1-12 </span> out of 36 products
+        </p>
+        <div className="px-3 py-5  shopcategory-sort ">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="mx-10 my-[110px] grid grid-cols-4 gap-5 ">
+        {all_products.map((item, i) => {
+          if (item.category === 'men') {
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />;
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <div className="flex justify-center items-center mx-auto my-[70px] w-[180px] h-[59px] rounded-[75px] bg-[#ededed] text-[#787878] text-[18px] font-medium">
+        Explore more
+      </div>
     </div>
   )
 }
--- src/Utils/ShopCategory.jsx
+++ src/Utils/ShopCategory.jsx
@@ -1,12 +1,30 @@
 import { useContext } from "react"
 import { ShopContext } from "../Context/ShopContext"
+import dropdown_icon from '../Images/dropdown_icon.png'
+import Item  from "../../src/Components/Items/Items"
 
 
 export const ShopCategory = (props) => {
     const {all_products} = useContext(ShopContext);
   return (
     <div className="shop-category">
-  
+     <div className="shop-category-index-short">
+      <p>
+        <span>Showing 1-12 </span> out of 36 products
+      </p>
+      <div className="shopcategory-short">
+        Sort by <img src={dropdown_icon} alt="" />
+      </div>
+     </div>
+     <div className="shopcategory-products">
+      {all_products.map((item,i) => {
+        if(props.category===item.category){
+          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />;
+        }else{
+          return null;
+        }
+      })}
+     </div>
     </div>
   )
 }
